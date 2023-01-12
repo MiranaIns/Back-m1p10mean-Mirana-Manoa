e188c062e8e805417571d9d0007d579c192b770c
manoaiharivola <manoaiharivola@gmail.com>
2023-01-13 01:13:10 +0300

    config database

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient
 const app = express();
 const configureRouter = require('./config/router.config')
 
+const connectionString = 'mongodb+srv://mongo:mongo@cluster0.ih3purh.mongodb.net/?retryWrites=true&w=majority'
 
-/* Middlewares */
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+    .then(client => {
+        console.log('Connected to Database ...');
+        const db = client.db('m1p10mean');
 
-/* Router */
-configureRouter(app);
+        /* Middlewares */
+        app.use(bodyParser.urlencoded({extended: true}));
+        app.use(bodyParser.json());
 
-app.listen(3000, function() {
-    console.log('listening on 3000');
-});
+        /* Router */
+        configureRouter(app);
+
+        app.listen(3000, function () {
+            console.log('Listening on 3000 ...');
+        });
+    })
+    .catch(console.error)
