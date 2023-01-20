ab00da38fd7b683c2ddd151acc6b0fd7ea3cdd77
manoaiharivola <manoaiharivola@gmail.com>
2023-01-21 02:17:36 +0300

    fix : response

--- controllers/authentification.controller.js
+++ controllers/authentification.controller.js
@@ -11,7 +11,7 @@ async function login(req,res){
         const db = req.app.get('db');
         let {mail, mdp} = req.body;
         let authToken = await AuthentificationService.login(db, mail,mdp);
-        res.json(normalizeApiResponse({status: httpStatus.OK, data: authToken})).status(httpStatus.OK);
+        res.json(normalizeApiResponse({status: httpStatus.OK, data: [authToken]})).status(httpStatus.OK);
     }
     catch(e){
         res.json(normalizeApiResponse({errors: e.message,status: httpStatus.UNAUTHORIZED})).status(httpStatus.OK);
