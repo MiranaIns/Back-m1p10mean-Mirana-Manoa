7cd74204202160bf62166d6de3a25d4e3f024180
manoaiharivola <manoaiharivola@gmail.com>
2023-01-24 20:07:58 +0300

    feat : register and login RFI

--- services/adminAuthentification.service.js
+++ services/adminAuthentification.service.js
@@ -3,6 +3,8 @@ const TokenService = require('./token.service');
 const Database = require("../database");
 const RatConnexionService = require("./ratConnexion.service");
 const RatService = require("./rat.service");
+const RfiConnexionService = require("./rfiConnexion.service");
+const RfiService = require("./rfi.service");
 
 const db = Database.getInstance();
 const client = Database.getMongoClient();
@@ -22,6 +24,17 @@ async function login(mail, motDePasse, utilisateurType){
             let motDePasseHash = bcrypt.hashSync(motDePasse, utilisateurConnexion.utilisateur_connexion_mdp_hash);
             if (utilisateurConnexion.utilisateur_connexion_mdp_hash === motDePasseHash) {
                 let utilisateur = await RatConnexionService.findUtilisateurByUtilisateurConnexionId(utilisateurConnexion._id);
+                return TokenService.generateAuthTokens(utilisateur);
+            } else throw new Error("Invalid email or password");
+        }
+        else if(utilisateurType === 'RFI') {
+            let utilisateurConnexion = await RfiConnexionService.findUtilisateurConnexionByEmail(mail.toLowerCase());
+            if (!utilisateurConnexion) {
+                throw new Error("Invalid email or password")
+            }
+            let motDePasseHash = bcrypt.hashSync(motDePasse, utilisateurConnexion.utilisateur_connexion_mdp_hash);
+            if (utilisateurConnexion.utilisateur_connexion_mdp_hash === motDePasseHash) {
+                let utilisateur = await RfiConnexionService.findUtilisateurByUtilisateurConnexionId(utilisateurConnexion._id);
                 return TokenService.generateAuthTokens(utilisateur);
             } else throw new Error("Invalid email or password");
         }
@@ -51,6 +64,29 @@ async function register(utilisateur){
                     const hash = bcrypt.hashSync(mdp,salt);
                     const utilisateurConnexionId = await RatConnexionService.create(mail.toLowerCase(),salt,hash, session);
                     const utilisateurId = await RatService.create(nom, prenom, mail.toLowerCase(),utilisateurConnexionId,session);
+                    await session.commitTransaction();
+                } catch (e) {
+                    await session.abortTransaction();
+                    throw e;
+                } finally {
+                    await session.endSession();
+                }
+            });
+        }
+        else if(utilisateurType === 'RFI') {
+            let utilisateurConnexion = await RfiConnexionService.findUtilisateurConnexionByEmail(mail.toLowerCase());
+            if(utilisateurConnexion) {
+                throw new Error("Account already exists");
+            }
+
+            client.then(async client => {
+                const session = await client.startSession();
+                session.startTransaction();
+                try {
+                    const salt = bcrypt.genSaltSync();
+                    const hash = bcrypt.hashSync(mdp,salt);
+                    const utilisateurConnexionId = await RfiConnexionService.create(mail.toLowerCase(),salt,hash, session);
+                    const utilisateurId = await RfiService.create(nom, prenom, mail.toLowerCase(),utilisateurConnexionId,session);
                     await session.commitTransaction();
                 } catch (e) {
                     await session.abortTransaction();
