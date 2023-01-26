a522194f364ca142dbc8a484b0220eefb118248b
manoaiharivola <manoaiharivola@gmail.com>
2023-01-26 18:06:21 +0300

    fix : delete test

--- config/router.config.js
+++ config/router.config.js
@@ -1,9 +1,7 @@
-const TestRouter = require('../routes/test/Test.route')
 const AuthentificationRouter = require('../routes/authentification.route')
 const AdminAuthentificationRouter = require('../routes/adminAuthentification.route')
 const VoitureRouter = require("../routes/utilisateur/voiture.route")
 const configureRouter = (app) => {
-    app.use('/', TestRouter);
     app.use('/authentification', AuthentificationRouter)
     app.use('/admin/authentification', AdminAuthentificationRouter)
 
